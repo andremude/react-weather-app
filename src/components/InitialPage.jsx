6d9eec6909d77183ac88c07fcf9a182f91693455
Andres Munoz <andremude@hotmail.com>
2022-09-11 19:32:16 -0500

    Initial Page & Image

--- src/components/InitialPage.jsx
+++ src/components/InitialPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import '../styles/InitialPage.css'
 import Clouds from "../images/initial-image.jpg"
 
 const InitialPage = () => {
   return (
-    <div className='initial-page-container'>
+    <div className='initial-image-container'>
       <img className='initial-image' src={Clouds} alt="clouds" />
       <h1 className='top-header-image'>
         <span className='soft'>Weather</span> App

11bbef66a7585a2a62e5072247b616002c6995bc
Andres Munoz <andremude@hotmail.com>
2022-09-11 19:02:09 -0500

    Initial Page

---
+++ src/components/InitialPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Clouds from "../../public/images/initial-image.jpg"
+
+const InitialPage = () => {
+  return (
+    <div className='initial-page-container'>
+      <img className='initial-image' src={Clouds} alt="clouds" />
+      <h1 className='top-header-image'>
+        <span className='soft'>Weather</span> App
+      </h1>
+    </div>
+  )
+}
+
+export default InitialPage
